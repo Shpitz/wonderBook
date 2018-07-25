dc296d5a2634ede43c5da0dfc3408e63cab17e01
omers <omer3026@gmail.com>
2018-07-25 15:56:12 +0300

    qeury in store

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,14 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import bookModule from './book-module.js'
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-
+  modules: {
+    bookModule
   },
+
   mutations: {
 
   },
