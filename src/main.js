5614051e52553668ea6c37c40f7ebaac7b036b57
Itzik Turjeman <itzikit@gmail.com>
2018-07-30 17:36:34 +0300

    editor component changes

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store/store';
-import './plugins/vuetify.js'
+// import './plugins/vuetify.js'
 import './registerServiceWorker';
 import './filters.js'
 import './assets/scss/style.scss';
+import vSelect from 'vue-select'
+Vue.component('v-select', vSelect)
 
 
 import { library } from '@fortawesome/fontawesome-svg-core'
@@ -15,9 +17,9 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 library.add(faEdit, faEnvelope, faBell, faBook, faCalendarAlt, faEye, faMapMarkerAlt,
   faClock, faStar, faForward,
   faBackward)
-
-Vue.component('font-awesome-icon', FontAwesomeIcon)
-Vue.config.productionTip = false;
+  
+  Vue.component('font-awesome-icon', FontAwesomeIcon)
+  Vue.config.productionTip = false;
 
 new Vue({
   router,
